1b295574dfe41289cc471694d9ef38f52c33ff0f
glorijamili <milikicglorija@gmail.com>
2026-02-23 22:45:25 +0100

    Fix: send Authorization header when loading hive for edit

--- proxy.ts
+++ proxy.ts
@@ -8,7 +8,7 @@ const ALLOWED_ORIGINS = new Set([
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "https://internet-tehnologije-2025-veb-aplikacija.onrender.com",
-  "https://honeyflow.onrender.com",,
+  "https://honeyflow.onrender.com",
 ]);
 
 function addSecurityHeaders(res: NextResponse) {
